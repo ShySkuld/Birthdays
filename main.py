50e6a8a59b1888a6d5f0520ae2ecd2ec2ee6e1f7
Skuld <ry-work@bk.ru>
2023-03-09 16:59:44 +0300

    Init

---
+++ main.py
@@ -0,0 +1,35 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QDialog, QMessageBox
+from ui_imagedialog import Ui_ImageDialog
+
+app = QApplication(sys.argv)
+window = QDialog()
+ui = Ui_ImageDialog()
+ui.setupUi(window)
+
+window.show()
+
+'''Окно ошибки'''
+msg = QMessageBox().critical(window,
+                             'VahVahTitle',
+                             'Some Info',
+                             QMessageBox.StandardButton.Ok
+                             | QMessageBox.StandardButton.Cancel,
+                             )
+print(msg)
+
+'''Всплывающее окно'''
+msgBox = QMessageBox()
+msgBox.setWindowTitle('My Super Title')
+msgBox.setText("The document has been modified.")
+msgBox.setInformativeText("Do you want to save your changes?")
+msgBox.setStandardButtons(
+    QMessageBox.StandardButton.Save | QMessageBox.StandardButton.Discard | QMessageBox.StandardButton.Cancel)
+msgBox.setDefaultButton(QMessageBox.StandardButton.Save)
+ret = msgBox.exec()
+print(ret)
+
+sys.exit(app.exec())
+
+txt = 'Hello World!'
+print(txt)
